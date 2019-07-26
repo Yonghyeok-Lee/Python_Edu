a74c936d5c374e655139bdc533e12ae56b0bdcee
zython-repo <dydgur316@gmail.com>
2019-07-26 18:01:14 +0900

    results list make great!

--- AppTest.py
+++ AppTest.py
@@ -3,59 +3,137 @@ from tkinter import ttk
 import random
 
 # funtion
+lists = []
 results = []
 kids = []
-ran = []
+pick = []
+sav = {}
+
+count = 0
+
+def makeResults(pick):
+    global lists
+    global results
+    global count
+    
+    results = []
+    for i in lists:
+        results.append(i)
+    if pick:
+        for i in pick:
+            if i in results:
+                results.remove(i)
+
+    print(results)
+
+def Produce101():
+    global count
+    global pick
+    global kids
+    global sav
+
+    pick.append(random.choice(kids))
+    
+    print('count: ', count, 'pick: ', pick[count])
+    
+    count += 1
+
+    print(len(kids), pick)
 
 def Gacha():
+    global lists
     global results
-    if not results:
-        label1.config(text="Go Edit")
-    else:
-        if len(results) != 10000:
-            a = 10000/len(results)
-            for i in results:
-                for j in range(0, int(a-1)):
-                    kids.append(i)
-            get = results + kids
+    global kids
+    global count
 
-        ran.append(random.choice(get))
-        ran.reverse()
-        per = get.count(ran[0]) * 100 / 10000
+    if not lists:
+        label1.config(text="Go Edit")
+
+    else:
+        kids = []
+        stock = int(10000/len(lists))
+        penalty = int(stock*0.05)
+        chance = int(stock*0.01)
+
+        makeResults(pick)
+
+        if count == 0:
+            for i in lists:
+                for j in range(stock):
+                    kids.append(i)
+            
+        elif count == 1:
+            kids = []
+
+            pick_cal = stock - penalty
+            results_cal = int((10000-pick_cal)/len(results))
+
+            for i in range(pick_cal):
+                kids.append(pick[count-1])
+
+            for i in results:
+                for j in range(results_cal):
+                    kids.append(i)
+
+        else:
+            kids = []
+
+            double_cal = stock - penalty - chance
+            chance_cal = stock - chance
+            results_cal = int((10000-double_cal)/len(lists))
+
+            print(len(lists), len(results), double_cal, chance_cal, results_cal)
+
+            for i in pick:
+                for j in range(chance_cal):
+                    kids.append(i)
+                print('append pick: ', len(kids)) 
+
+            for i in results:
+                for j in range(int(results_cal/len(results))):
+                    kids.append(i)
+                print('append results: ', len(kids)) 
+
+            if pick[count-1] == pick[count-2]:
+                for j in range(double_cal):
+                    kids.append(pick[count-1])
+                print('append other: ', len(kids))
+
+        Produce101()
+
         sav = open("C:/Users/yh/Desktop/Eatcha.txt", "a")
-        for i in ran:
+        for i in lists:
             data = "name: " + i + "\n"
             sav.write(data)
         sav.close()
 
-        label1.config(text=str(ran[0]))
+        label1.config(text=str(pick[count-1]))
 
 def addList():
-    global results
-    results.append(entry.get())
+    global lists
+    lists.append(entry.get())
     if not entry.get():
-        return results.pop()
+        return lists.pop()
     listbox.insert(END, entry.get())
     entry.delete(0, "end")
 
 def delList():
-    global results
+    global lists
     selection = listbox.curselection()
     if(len(selection)==0):
         return
     value = listbox.get(selection[0])
-    index = results.index(value)
+    index = lists.index(value)
     length = len(selection)
-    del results[index:]
+    del lists[index:]
     listbox.delete(selection[0], (length+1))
-    print(results)
 
 def savList():
-    global results
-    if not results:
+    global lists
+    if not lists:
         return buttonSav.config(text="No List")
     sav = open("C:/Users/yh/Desktop/Eatcha.txt", "a")
-    for i in results:
+    for i in lists:
         data = "name: " + i + "\n"
         sav.write(data)
     sav.close()
@@ -89,9 +167,9 @@ label2 = Label(frame2, text="Setting List", font=('arial', 12, 'normal'), padx=7
 label2.grid(row=0, column=0, sticky='N') 
 
 listbox = Listbox(frame2, selectmode="extended")
-for i in range(len(results)):
-    listbox.insert(END, results[i]),
-    listbox.insert(len(listbox), results[i])
+for i in range(len(lists)):
+    listbox.insert(END, lists[i]),
+    listbox.insert(len(listbox), lists[i])
 listbox.grid(row=0, column=2, sticky='N') 
 
 buttonAdd = Button(frame2, width=5, padx=2, text="Add", overrelief="solid", command=addList)
